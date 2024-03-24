33fab868afb1bdb2800f1663e4d6639bd956e35d
beegle98 <ckwjdans@ajou.ac.kr>
2024-03-24 17:58:29 +0900

    init

---
+++ D3/20019.cpp
@@ -0,0 +1,20 @@
+//회문의 회문
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    return 0;
+
+}
