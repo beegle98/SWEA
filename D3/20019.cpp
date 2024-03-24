60af6ba3040d260bc909b5f78b41ca93d124361c
beegle98 <ckwjdans@ajou.ac.kr>
2024-03-24 19:32:51 +0900

    회문의 회문

--- D3/20019.cpp
+++ D3/20019.cpp
@@ -7,14 +7,39 @@ typedef long long ll;
 typedef vector<ll> vll;
 typedef vector<int> vi;
 
+int n;
+string s;
+
+int palindrome(int l,int r){
+
+    int result=1;
+    for(int i=0;i<(r-l)/2;i++){
+        //cout << s[l+i] << " / " << s[r-1-i] << '\n';
+        if(s[l+i] != s[r-1-i]){
+            result = 0;
+            break;
+        }
+    }
+    return result;
+}
 int main(){
     ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-    
-    int n;
-    cin >> n;
-    
-    return 0;
 
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> s;
+        //cout << s << ' ';
+        int len=s.length();
+        int sum=0;
+        sum+=palindrome(0,len);
+        sum+=palindrome(0,(len-1)/2);
+        sum+=palindrome((len-1)/2+1,len);
+
+        cout << '#' << i+1 << ' ';
+        if(sum==3) cout << "YES" <<'\n';
+        else cout << "NO" << '\n';
+    }
+    return 0;
 }
