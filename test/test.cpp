8fc509210deebbe1d626da4ed8ccaac81e23e114
beegle98 <ckwjdans@ajou.ac.kr>
2024-04-18 22:44:31 +0900

    10988 solved

--- test/test.cpp
+++ test/test.cpp
@@ -10,17 +10,15 @@ typedef pair<ll,ll> pll;
 
 int main(){
     FASTIO
-    vector<string> s;
-    for(int i=0;i<5;i++){
-        string a;
-        cin >> a;
-        s.push_back(a);
-    }
-    for(int i=0;i<15;i++){
-        for(int j=0;j<5;j++){
-            if(s[j].size()<i+1) continue;
-            cout << s[j][i];
+    string s;
+    cin >> s;
+    int len=s.size();
+    for(int i=0;i<len;i++){
+        if(s[i]!=s[len-1-i]){
+            cout << "0\n";
+            return 0;
         }
     }
+    cout << "1\n";
     return 0;
 }
